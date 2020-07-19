811f4dce2283673b514df83b0c3062b41bb51d39
piyushsrii <piyushnamo888@gmail.com>
2020-07-19 20:56:51 +0530

    commit in uc6

--- 11thSortAssignment.sh
+++ 11thSortAssignment.sh
@@ -14,32 +14,21 @@ echo $c
 
 #!/bin/bash -x     #UserCase----> 2
 
-read a
-read b
-read c
 Compute=$(($a+$(($b*$c))))
 echo $Compute
 
 #!/bin/bash -x     #UserCase----> 3
 
-read a
-read b
-read c
 Compute1=$(($(($a*$b))+$c))
 echo $compute1
 
 #!/bin/bash -x      #UserCase----> 4
 
-read a
-read b
-read c
 compute2=$(($a+$(($b/$c))))
 echo $compute2
 
 #!/bin/bash -x      #UserCase---->5
-read a
-read b
-read c
+
 Compute3=$(($(($a%$b))+$c))
 echo $Compute3
 
@@ -47,9 +36,6 @@ echo $Compute3
 
 declare -A Dictionary;
 
-read a
-read b
-read c
 function Dic () {
 #local $var=$1
 case $1 in
