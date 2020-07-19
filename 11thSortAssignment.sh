d4206397102807db6494fbaa3f56857756665d18
piyushsrii <piyushnamo888@gmail.com>
2020-07-19 18:46:24 +0530

    commit in uc6

--- 11thSortAssignment.sh
+++ 11thSortAssignment.sh
@@ -27,3 +27,57 @@ read b
 read c
 Compute1=$(($(($a*$b))+$c))
 echo $compute1
+
+#!/bin/bash -x      #UserCase----> 4
+
+read a
+read b
+read c
+compute2=$(($a+$(($b/$c))))
+echo $compute2
+
+#!/bin/bash -x      #UserCase---->5
+read a
+read b
+read c
+Compute3=$(($(($a%$b))+$c))
+echo $Compute3
+
+#!/bin/bash -x       #UserCase---->6
+
+declare -A Dictionary;
+
+read a
+read b
+read c
+function Dic () {
+#local $var=$1
+case $1 in
+1)
+Compute=$(($a+$(($b*$c))))
+     echo $Compute
+        ;;
+2)
+Compute1=$(($(($a*$b))+$c))
+         echo $Compute1
+        ;;
+3)
+Compute2=$(($a+$(($b/$c))))
+        echo $Compute2
+         ;;
+4)
+Compute3=$(($(($a%$b))+$c))
+echo $Compute3
+  ;;
+*)
+      echo "no value"
+       ;;
+esac
+}
+for((i=1;i<=4;i++))
+do
+var=$i
+Dictionary[$i]=$(Dic $var)
+echo ${Dictinary[i]}
+done
+
