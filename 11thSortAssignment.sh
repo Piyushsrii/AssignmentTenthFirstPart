3fe8616166366fc683dd8397d59d87e1d73d0a55
piyushsrii <piyushnamo888@gmail.com>
2020-07-19 20:53:26 +0530

    commit in uc4

--- 11thSortAssignment.sh
+++ 11thSortAssignment.sh
@@ -14,23 +14,15 @@ echo $c
 
 #!/bin/bash -x     #UserCase----> 2
 
-read a
-read b
-read c
 Compute=$(($a+$(($b*$c))))
 echo $Compute
 
 #!/bin/bash -x     #UserCase----> 3
 
-read a
-read b
-read c
 Compute1=$(($(($a*$b))+$c))
 echo $compute1
 
-#!/bin/bash -x
-read a
-read b
-read c
+#!/bin/bash -x     #UserCase----->4
+
 Compute2=$(($a+$(($b/$c))))
 echo $compute2
