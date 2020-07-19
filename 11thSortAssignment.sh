2f29cff439bb351a781cb57aac8d8716deb8b2fc
piyushsrii <piyushnamo888@gmail.com>
2020-07-19 18:58:05 +0530

    commit in uc5

--- 11thSortAssignment.sh
+++ 11thSortAssignment.sh
@@ -28,9 +28,17 @@ read c
 Compute1=$(($(($a*$b))+$c))
 echo $compute1
 
-#!/bin/bash -x
+#!/bin/bash -x     #UserCase----> 4
+
 read a
 read b
 read c
 Compute2=$(($a+$(($b/$c))))
 echo $compute2
+
+#!/bin/bash -x
+read a
+read b
+read c
+Compute3=$(($(($a%$b))+$c))
+echo $Compute3
